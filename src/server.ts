6f83a25098069185e7d9f9751e3e6bc1bfb1a23e
jjensen-ai <jordon.jensen@live.ca>
2023-04-09 20:12:52 -0400

    one last time i guess jesus christ

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 // Imports for node modules
-import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 // Imports for API
 
@@ -19,26 +18,19 @@ const SERVER_PORT = process.env.PORT;
 
 //Start my Server
 async function serverStart() {
-  const app = express();
-
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
 
-  await server.start();
-
-  app.use('/graphql');
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 3030 },
+  });
 
   await mongoose.connect(`${DB}`).then(() => {
     console.log('DB CONNECTED');
   });
 
-  app.listen(SERVER_PORT, () => {
-    console.log(`Server running at port ${SERVER_PORT}`);
-  });
+  console.log(`Sever is ready at ${url}`)
 }
 serverStart();
